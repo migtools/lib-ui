3a314d56a8df96363047ca79427059b713c2b329
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-24 15:12:02 -0400

    Remove dependence on mig-ui redux state structure (BREAKING)

--- src/modules/kube-client/client_factory.ts
+++ src/modules/kube-client/client_factory.ts
@@ -12,108 +12,83 @@ export class ClientFactoryUnknownClusterError extends Error {
 
 export class ClientFactoryMissingUserError extends Error {
   constructor() {
-    super('Current user missing from state tree');
+    super('Current user missing from client factory');
     Object.setPrototypeOf(this, ClientFactoryMissingUserError.prototype);
   }
 }
 
 export class ClientFactoryMissingApiRoot extends Error {
   constructor() {
-    super('apiRoot missing from migMeta');
+    super('Cluster API URL missing from client factory');
     Object.setPrototypeOf(this, ClientFactoryMissingUserError.prototype);
   }
 }
 
 export class ClientFactoryMissingDiscoveryApi extends Error {
   constructor() {
-    super('migMeta.discoveryApi is missing');
+    super('Discovery API URL missing from client factory');
     Object.setPrototypeOf(this, ClientFactoryMissingDiscoveryApi.prototype);
   }
 }
 
-// TODO use individual args instead of mig-ui-coupled state structure
-interface IMigState {
-  auth: {
-    user: { access_token: string; expiry_time: number };
-    migMeta: { clusterApi: string; discoveryApi: string; namespace: string };
-  };
-  token: {
-    tokenList: {
-      MigToken: {
-        spec: {
-          migClusterRef: {
-            name: string;
-          };
-        };
-      };
-      Secret: {
-        data: {
-          token: string;
-        };
-      };
-    }[];
-  };
+interface IUser {
+  access_token: string;
+  expiry_time: number;
 }
 
 let tokenExpiryHandler: TokenExpiryHandler | null = null;
 export const ClientFactory = {
-  cluster: (state: IMigState, customResponseType: ResponseType = 'json'): ClusterClient => {
-    if (!state.auth.user) {
+  cluster: (
+    user: IUser,
+    clusterApi: string,
+    customResponseType: ResponseType = 'json'
+  ): ClusterClient => {
+    if (!user) {
       throw new ClientFactoryMissingUserError();
     }
-    if (!state.auth.migMeta.clusterApi) {
+    if (!clusterApi) {
       throw new ClientFactoryMissingApiRoot();
     }
 
-    const newClient = new ClusterClient(
-      state.auth.migMeta.clusterApi,
-      state.auth.user.access_token,
-      customResponseType
-    );
+    const newClient = new ClusterClient(clusterApi, user.access_token, customResponseType);
 
     if (tokenExpiryHandler) {
       newClient.setTokenExpiryHandler(
         (tokenExpiryHandler as unknown) as TokenExpiryHandler,
-        state.auth.user.expiry_time
+        user.expiry_time
       );
     }
 
     return newClient;
   },
   discovery: (
-    state: IMigState,
+    user: IUser,
+    namespace: string,
+    discoveryApi: string,
     clusterName?: string,
+    tokenSecret?: string,
     customResponseType: ResponseType = 'json'
-  ): DiscoveryClient => {
-    if (!state.auth.user) {
+  ): DiscoveryClient | null => {
+    if (!user) {
       throw new ClientFactoryMissingUserError();
-    }
-    if (!state.auth.migMeta.discoveryApi) {
-      // TODO make discovery API optional
-      throw new ClientFactoryMissingDiscoveryApi();
     }
 
     let decodedToken: string | null = null;
-    if (clusterName) {
-      const matchingToken = state.token.tokenList.find(
-        (token) => token.MigToken.spec.migClusterRef.name === clusterName
-      );
-      if (matchingToken) {
-        const { token } = matchingToken.Secret.data;
-        decodedToken = atob(token);
-      }
+    if (clusterName && tokenSecret) {
+      decodedToken = atob(tokenSecret);
     }
+
     const discoveryClient = new DiscoveryClient(
-      state.auth.migMeta.discoveryApi,
-      state.auth.migMeta.namespace,
-      decodedToken ? decodedToken : state.auth.user.access_token,
+      discoveryApi,
+      namespace,
+      decodedToken ? decodedToken : user.access_token,
       customResponseType
     );
 
     if (tokenExpiryHandler) {
       discoveryClient.setTokenExpiryHandler(
         (tokenExpiryHandler as unknown) as TokenExpiryHandler,
-        state.auth.user.expiry_time
+        user.expiry_time
       );
     }
     return discoveryClient;
