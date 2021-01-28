f369bc5cb44d052d894ee6fb50a3709e5f79e677
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-01-28 12:26:42 -0500

    Add usePaginationState and useSortState from forklift-ui (add generics to pagination)

---
+++ src/hooks/usePaginationState/usePaginationState.test.ts
@@ -0,0 +1,9 @@
+// import { usePaginationState } from './usePaginationState';
+
+// TODO add tests here
+
+describe('usePaginationState', () => {
+  it.skip('works', () => {
+    // TODO
+  });
+});
---
+++ src/hooks/useSortState/useSortState.test.ts
@@ -0,0 +1,9 @@
+// import { useSortState } from './useSortState';
+
+// TODO add tests here
+
+describe('useSortState', () => {
+  it.skip('works', () => {
+    // TODO
+  });
+});
