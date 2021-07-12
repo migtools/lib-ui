bc5ab01fb13dd5508065d59715378a04cce819d2
Ian Bolton <ibolton@redhat.com>
2021-07-12 18:46:21 -0400

    fix(discovery): remove unused shared discovery service code
    This code is only relevant to MTC and should be removed.

--- src/modules/kube-client/client_factory.ts
+++ src/modules/kube-client/client_factory.ts
@@ -1,5 +1,4 @@
 import { ClusterClient } from './client';
-import { DiscoveryClient } from './discoveryClient';
 import { ResponseType } from 'axios';
 import { TokenExpiryHandler } from './resources/common';
 
@@ -21,13 +20,6 @@ export class ClientFactoryMissingApiRoot extends Error {
   constructor() {
     super('Cluster API URL missing from client factory');
     Object.setPrototypeOf(this, ClientFactoryMissingUserError.prototype);
-  }
-}
-
-export class ClientFactoryMissingDiscoveryApi extends Error {
-  constructor() {
-    super('Discovery API URL missing from client factory');
-    Object.setPrototypeOf(this, ClientFactoryMissingDiscoveryApi.prototype);
   }
 }
 
@@ -60,38 +52,6 @@ export const ClientFactory = {
     }
 
     return newClient;
-  },
-  discovery: (
-    user: IUser,
-    namespace: string,
-    discoveryApi: string,
-    clusterName?: string,
-    tokenSecret?: string,
-    customResponseType: ResponseType = 'json'
-  ): DiscoveryClient | null => {
-    if (!user) {
-      throw new ClientFactoryMissingUserError();
-    }
-
-    let decodedToken: string | null = null;
-    if (clusterName && tokenSecret) {
-      decodedToken = atob(tokenSecret);
-    }
-
-    const discoveryClient = new DiscoveryClient(
-      discoveryApi,
-      namespace,
-      decodedToken ? decodedToken : user.access_token,
-      customResponseType
-    );
-
-    if (tokenExpiryHandler) {
-      discoveryClient.setTokenExpiryHandler(
-        (tokenExpiryHandler as unknown) as TokenExpiryHandler,
-        user.expiry_time
-      );
-    }
-    return discoveryClient;
   },
 };
 
