bee827771f3cd17fe1fbb453fffeadf4c6491957
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-07-29 13:37:36 -0400

    Improve docs

--- src/hooks/useTableSortState/useTableSortState.stories.tsx
+++ src/hooks/useTableSortState/useTableSortState.stories.tsx
@@ -1,5 +1,4 @@
 import {
-  ICell,
   sortable,
   Table,
   TableBody,
--- src/hooks/useTableSortState/useTableSortState.ts
+++ src/hooks/useTableSortState/useTableSortState.ts
@@ -1,27 +1,27 @@
 import * as React from 'react';
 import { ISortBy, SortByDirection } from '@patternfly/react-table';
 
-interface IBaseTableSortStateParams<T> {
+interface IBaseTableSortStateArgs<T> {
   items: T[];
   initialSortColumnIndex?: number | null;
   initialSortDirection?: 'asc' | 'desc';
 }
 
-interface ITableSortStateParamsByValue<T> extends IBaseTableSortStateParams<T> {
+interface ITableSortStateArgsByValue<T> extends IBaseTableSortStateArgs<T> {
   getSortValues: (item: T) => (string | number | boolean)[];
   compareFn?: never;
 }
 
-interface ITableSortStateParamsByFunction<T> extends IBaseTableSortStateParams<T> {
+interface ITableSortStateArgsByFunction<T> extends IBaseTableSortStateArgs<T> {
   getSortValues?: never;
   compareFn: (itemA: T, itemB: T, sortColumnIndex: number, sortDirection: 'asc' | 'desc') => number;
 }
 
-export type TableSortStateParams<T> =
-  | ITableSortStateParamsByValue<T>
-  | ITableSortStateParamsByFunction<T>;
+export type TableSortStateArgs<T> =
+  | ITableSortStateArgsByValue<T>
+  | ITableSortStateArgsByFunction<T>;
 
-export interface ISortStateHook<T> {
+export interface ITableSortStateHook<T> {
   sortedItems: T[];
   sortColumnIndex: number | null;
   setSortColumnIndex: React.Dispatch<React.SetStateAction<number | null>>;
@@ -34,8 +34,8 @@ export interface ISortStateHook<T> {
   };
 }
 
-export const useTableSortState = <T>(params: TableSortStateParams<T>): ISortStateHook<T> => {
-  const { items, initialSortColumnIndex = null, initialSortDirection = 'asc' } = params;
+export const useTableSortState = <T>(args: TableSortStateArgs<T>): ITableSortStateHook<T> => {
+  const { items, initialSortColumnIndex = null, initialSortDirection = 'asc' } = args;
   const [sortColumnIndex, setSortColumnIndex] = React.useState<number | null>(
     initialSortColumnIndex
   );
@@ -44,12 +44,12 @@ export const useTableSortState = <T>(params: TableSortStateParams<T>): ISortStat
   let sortedItems = items;
   if (sortColumnIndex !== null) {
     let sortFn: (itemA: T, itemB: T) => number;
-    if (params.compareFn) {
-      sortFn = (a: T, b: T) => params.compareFn(a, b, sortColumnIndex, sortDirection);
+    if (args.compareFn) {
+      sortFn = (a: T, b: T) => args.compareFn(a, b, sortColumnIndex, sortDirection);
     } else {
       sortFn = (a: T, b: T) => {
-        const aValue = params.getSortValues(a)[sortColumnIndex || 0];
-        const bValue = params.getSortValues(b)[sortColumnIndex || 0];
+        const aValue = args.getSortValues(a)[sortColumnIndex || 0];
+        const bValue = args.getSortValues(b)[sortColumnIndex || 0];
         if (aValue < bValue) return sortDirection === 'asc' ? -1 : 1;
         if (aValue > bValue) return sortDirection === 'asc' ? 1 : -1;
         return 0;
