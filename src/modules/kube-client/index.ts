66ce1b072b4f057862818e9b0f0e6419e3f67886
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-08-23 12:59:22 -0400

    feat(index): add warnings about moving package to `@migtools/lib-ui` (#110)

--- src/modules/kube-client/index.ts
+++ src/modules/kube-client/index.ts
@@ -1,4 +1,8 @@
+import { packageMovedConsoleWarning } from '../../common/packageMovedConsoleWarning';
+
 export * from './client';
 export * from './client_factory';
 export * from './resources/common';
 export * from './resources/core';
+
+console.warn(packageMovedConsoleWarning);
