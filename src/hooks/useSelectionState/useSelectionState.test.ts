72492043a5955f2d934dad0433516b0afff4f540
ibolton336 <ibolton@redhat.com>
2022-12-07 17:07:23 -0500

    Add tests for useSelectionState hook
    Move testing library util function dep to dev deps
    
    Signed-off-by: Ian Bolton <ibolton@redhat.com>

--- src/hooks/useSelectionState/useSelectionState.test.ts
+++ src/hooks/useSelectionState/useSelectionState.test.ts
@@ -1,21 +1,83 @@
-// import { useSelectionState } from './useSelectionState';
+import { act, render } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 
-// TODO add tests here
+import { useSelectionState } from './useSelectionState';
+
+interface IFruit {
+  name: string;
+}
+const fruits: IFruit[] = [{ name: 'Apple' }, { name: 'Orange' }, { name: 'Banana' }];
 
 describe('useSelectionState', () => {
-  it.skip('updates state correctly when toggling an item', () => {
-    // TODO
+  it('Initializes selection state and hook loads with no errors', () => {
+    const { result } = renderHook(() => useSelectionState<IFruit>({ items: fruits }));
+    expect(result.current.selectedItems).toEqual([]);
   });
-  it.skip('updates state correctly when selecting/deselecting all', () => {
-    // TODO
+
+  it('Updates state correctly when toggling an item', () => {
+    const { result } = renderHook(() =>
+      useSelectionState<String>({
+        items: ['A', 'B', 'C'],
+      })
+    );
+
+    act(() => result.current.toggleItemSelected('A', true));
+    expect(result.current.isItemSelected('A')).toBe(true);
   });
-  it.skip('preserves the original order of items when selecting out of order', () => {
-    // TODO
+
+  it('Updates state correctly when setting selected state', () => {
+    const { result } = renderHook(() =>
+      useSelectionState<String>({
+        items: ['A', 'B', 'C'],
+      })
+    );
+
+    act(() => result.current.setSelectedItems(['A']));
+    expect(result.current.selectedItems).toEqual(['A']);
   });
-  it.skip('handles initialSelected properly', () => {
-    // TODO
+
+  it('Updates state correctly when selecting/deselecting all', () => {
+    const { result } = renderHook(() =>
+      useSelectionState<String>({
+        items: ['A', 'B', 'C'],
+      })
+    );
+    act(() => result.current.selectAll(true));
+    expect(result.current.areAllSelected).toEqual(true);
+    expect(result.current.selectedItems).toEqual(['A', 'B', 'C']);
+
+    act(() => result.current.selectAll(false));
+    expect(result.current.areAllSelected).toEqual(false);
+    expect(result.current.selectedItems).toEqual([]);
   });
-  it.skip('handles a custom isEqual arg properly', () => {
-    // TODO
+
+  it('Preserves the original order of items when selecting out of order', () => {
+    const { result } = renderHook(() => useSelectionState<IFruit>({ items: fruits }));
+    act(() =>
+      result.current.setSelectedItems([{ name: 'Orange' }, { name: 'Apple' }, { name: 'Banana' }])
+    );
+    expect(result.current.selectedItems).toEqual(fruits);
+  });
+
+  it('Handles initialSelected properly', () => {
+    const { result, rerender } = renderHook(() =>
+      useSelectionState<String>({
+        items: ['A', 'B', 'C'],
+        initialSelected: ['A'],
+      })
+    );
+    rerender();
+    expect(result.current.selectedItems).toEqual(['A']);
+  });
+
+  it('Handles a custom isEqual arg properly', () => {
+    const { result } = renderHook(() =>
+      useSelectionState<IFruit>({
+        items: fruits,
+        isEqual: (a, b) => a.name === b.name,
+      })
+    );
+    act(() => result.current.setSelectedItems([{ name: 'Apple' }]));
+    expect(result.current.selectedItems).toEqual([{ name: 'Apple' }]);
   });
 });
