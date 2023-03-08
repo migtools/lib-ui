f0efdaadf9039b3f27282959d57a89bcb62e9d8d
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-03-08 13:59:55 -0500

    Type fixes, remove idle state
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- src/queries/helpers.ts
+++ src/queries/helpers.ts
@@ -4,6 +4,5 @@ import { UnknownResult } from '../common/types';
 export const getAggregateQueryStatus = (queryResults: UnknownResult[]): QueryStatus => {
   if (queryResults.some((result) => result.isError)) return 'error';
   if (queryResults.some((result) => result.isLoading)) return 'loading';
-  if (queryResults.every((result) => result.isIdle)) return 'idle';
   return 'success';
 };
